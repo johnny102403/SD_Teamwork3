f111f8ffb56f3859dce165d638b979ba6983f549
Cameradog <kay821221@gmail.com>
2015-08-31 16:48:52 +0800

    css fix

--- Cameradog's framwork/resources/views/main/show.blade.php
+++ Cameradog's framwork/resources/views/main/show.blade.php
@@ -8,17 +8,19 @@
     <div class="row">
         <div id="section1" class="container-fluid">
             <h1 class="text-center">Delete Message</h1>
-            <h2>{{$messages->name}}</h2>
-            <p>{{$messages->content}}</p>
-            <p>{{$messages->time}}</p>
-            <br>
-            <p>Do you want to delete this message?</p>
-            <button id='notDelete' class="btn btn-info">Maybe not</button>
-            <form action="{{ URL('main/'.$messages->id) }}" method="POST">
-                <input name='_method' type='hidden' value="DELETE">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <button class="btn btn-danger">Delete message</button>
-            </form>
+            <div id="sectionDelete">
+                <span class='userFont'>{{$messages->name}}</span>
+                <p class='contentFont'>{{$messages->content}}</p>
+                <p class='timeFont'>{{$messages->time}}</p>
+                <br>
+                <span class="deleteMs">Do you want to delete this message?</span>
+                <button id='notDelete' class="btn btn-info">Maybe not</button>
+                <form action="{{ URL('main/'.$messages->id) }}" method="POST">
+                    <input name='_method' type='hidden' value="DELETE">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <button class="btn btn-danger">Delete message</button>
+                </form>
+            </div>
         </div>
     </div>
 @stop
